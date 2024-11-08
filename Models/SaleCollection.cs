4ac7833ccc62c3e34027ef51cf2e9b5b9d24bca9
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-11-09 00:54:58 +0100

    SaleCollection - add salesElement enum and Open method

--- Models/SaleCollection.cs
+++ Models/SaleCollection.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace WeekendSales.Models
 {
@@ -15,11 +18,39 @@ namespace WeekendSales.Models
         public SaleCollection(string path)
         {
             _collection = new Collection<Sale>();
+            Open();
         }
 
         public Sale this[int index]
         {
             get => _collection[index];
+        }
+
+        private enum _salesElement
+        {
+            InvalidElement,
+            Sales,
+            EndSales,
+            Sale,
+            EndSale,
+            ItemName,
+            EndItemName,
+            Date,
+            EndDate,
+            Price,
+            EndPrice,
+            Tax,
+            EndTax
+        }
+
+        private void Open()
+        {
+
+        }
+
+        private _salesElement ReadElement(XmlReader reader, out string text)
+        {
+            throw new NotImplementedException();
         }
 
         public int Count => _collection.Count;
