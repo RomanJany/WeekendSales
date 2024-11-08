49eefa92608deda436c1c3cf0d97a036f173eb83
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-11-08 22:57:14 +0100

    add SaleCollection

---
+++ Models/SaleCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekendSales.Models
+{
+    public class SaleCollection
+    {
+        private Collection<Sale> _collection;
+        
+        public SaleCollection(string path)
+        {
+            _collection = new Collection<Sale>();
+        }
+    }
+}
