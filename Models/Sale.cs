4bed4d0a9343e0bd0afe277ad0ae71fe21860c19
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-11-08 21:08:03 +0100

    add Sale model

---
+++ Models/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekendSales.Models
+{
+    public class Sale
+    {
+        required public string ItemName { get; set; }
+        required public DateTime Date { get; set; }
+        required public double Price { get; set; }
+        required public double Tax { get; set; }
+
+        public void Open(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
