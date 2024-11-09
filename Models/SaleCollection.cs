108c261ac45ed0a9b93c3b1c76d3f96eeb4089a7
RomanJany <176539047+RomanJany@users.noreply.github.com>
2024-11-09 01:16:20 +0100

    SaleCollection - ReadElement implementation

--- Models/SaleCollection.cs
+++ Models/SaleCollection.cs
@@ -50,7 +50,63 @@ namespace WeekendSales.Models
 
         private _salesElement ReadElement(XmlReader reader, out string text)
         {
-            throw new NotImplementedException();
+            // Text
+            if (reader.NodeType == XmlNodeType.Text &&
+                reader.Depth == 3)
+            {
+                text = reader.Value;
+            }
+            else
+            {
+                text = "";
+            }
+
+            if (reader.NodeType == XmlNodeType.Element || reader.NodeType == XmlNodeType.EndElement)
+            {
+                // Sales element
+                if (reader.Depth == 0 &&
+                    reader.Name == "sales")
+                {
+                    return reader.NodeType == XmlNodeType.Element ? _salesElement.Sales : _salesElement.EndSales;
+                }
+
+                // Sale element
+                if (reader.Depth == 1 &&
+                    reader.Name == "sale")
+                {
+                    return reader.NodeType == XmlNodeType.Element ? _salesElement.Sale : _salesElement.EndSale;
+                }
+
+                if (reader.Depth == 2)
+                {
+                    // ItemName element
+                    if (reader.Name == "itemName")
+                    {
+                        return reader.NodeType == XmlNodeType.Element ? _salesElement.ItemName : _salesElement.EndItemName;
+                    }
+
+                    // Date element
+                    if (reader.Name == "date")
+                    {
+                        return reader.NodeType == XmlNodeType.Element ? _salesElement.Date : _salesElement.EndDate;
+                    }
+
+                    // Price element
+                    if (reader.Name == "price")
+                    {
+                        return reader.NodeType == XmlNodeType.Element ? _salesElement.Price : _salesElement.EndPrice;
+                    }
+
+                    // Tax element
+                    if (reader.Name == "tax")
+                    {
+                        return reader.NodeType == XmlNodeType.Element ? _salesElement.Tax : _salesElement.EndTax;
+                    }
+                }
+            }
+
+
+            return _salesElement.InvalidElement;
         }
 
         public int Count => _collection.Count;
